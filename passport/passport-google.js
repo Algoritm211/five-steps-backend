c5b57689e4bdd0c986dfd38ebd6de6984fd7cb8a
alexey_horbunov <algoritm211@gmail.com>
2021-04-24 12:55:52 +0300

    set mainURL in enviroment

--- passport/passport-google.js
+++ passport/passport-google.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/google/callback",
+      callbackURL: `${process.env.mainURL}/api/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       // console.log(profile)
